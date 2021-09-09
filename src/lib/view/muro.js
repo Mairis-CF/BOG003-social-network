c488dc489e2a1946384bc777074be61d9b9f80e8
Mairis Castro Florez <mairiscastroflorez@gmail.com>
2021-09-09 12:04:27 -0500

    Mostrando nombre de usuario

--- src/lib/view/muro.js
+++ src/lib/view/muro.js
@@ -1,23 +1,31 @@
-import { logOutUser } from '../index.js';
+import { logOutUser } from "../index.js";
 
 export const createTimeLineView = () => {
-  const timeLineSection = document.createElement('section');
+  const timeLineSection = document.createElement("section");
   const timeLineView = `  
       <h1> Bienvenido al Muro </h1>
       <button type="button" id ='logOut'> <img src="images/log-out1.png" alt="Log Out" /></button>
-  `
+      <div id="prueba"></div>
+  `;
   timeLineSection.innerHTML = timeLineView;
-  const logOut = timeLineSection.querySelector('#logOut');
+  const logOut = timeLineSection.querySelector("#logOut");
 
-  logOut.addEventListener('click', (e) => {
+  const user = firebase.auth().currentUser;
+  console.log(user);
+
+  const pruebaName = timeLineSection.querySelector("#prueba");
+  pruebaName.innerHTML = `Hola ${user.displayName}`;
+
+  logOut.addEventListener("click", (e) => {
     e.preventDefault();
     logOutUser()
-    .then(() => {
-     window.location.hash = '#/ingreso';
-    }).catch((error) => {
-    console.log(error);
-    });
-  })
+      .then(() => {
+        window.location.hash = "#/ingreso";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   return timeLineSection;
 };
