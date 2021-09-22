5be68929baae1b0e041d0a47a2f831d3b2ebc895
unknown <marlysrc@gmail.com>
2021-09-22 16:35:07 -0500

    editar actualizado

--- src/lib/view/muro.js
+++ src/lib/view/muro.js
@@ -1,10 +1,16 @@
 import {
-  savePost, getPosts, deletePost, updatePost, logOutUser, addLike, removeLike,
-} from '../index.js';
+  savePost,
+  getPosts,
+  deletePost,
+  updatePost,
+  logOutUser,
+  addLike,
+  removeLike,
+} from "../index.js";
 
 export const createTimeLineView = () => {
-  const timeLineSection = document.createElement('section');
-  timeLineSection.setAttribute('class', 'muro-section');
+  const timeLineSection = document.createElement("section");
+  timeLineSection.setAttribute("class", "muro-section");
   const timeLineView = `    
     <header class="header-muro">
       <p class="user-name" id="userName"></p>
@@ -23,10 +29,16 @@ export const createTimeLineView = () => {
         <button class="delete-confirm">Eliminar</button>
         <button class="cancel-delete">Cancelar</button>
     </div>
+    <div id="containerEdit" class="container-edit">
+      <div id="modalEdit" class="modal-edit">
+        <textarea id="editBox" name="edit"></textarea>
+        <button class="edit-confirm">Guardar</button>
+        <button class="cancel-edit">Cancelar</button>
+      </div>
+    </div>
     <form id="postForm">
       <textarea id="postBox" name="post" placeholder="Comparte tu experiencia..."></textarea>
       <button type="submit" id="btnPost" disabled>Publicar</button>
-      <button id="btnSave">Guardar</button>
     </form>
     <section class="post-section" id="postSection"></section>
   `;
@@ -35,67 +47,64 @@ export const createTimeLineView = () => {
   /* Mostrar el nombre del usuario en el header */
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      const userName = timeLineSection.querySelector('#userName');
+      const userName = timeLineSection.querySelector("#userName");
       userName.textContent = user.displayName;
     }
   });
 
   /* Guardar el post en la base de datos */
-  const postForm = timeLineSection.querySelector('#postForm');
-  const postBox = timeLineSection.querySelector('#postBox');
-  const btnPost = timeLineSection.querySelector('#btnPost');
-  
-  postBox.addEventListener('keyup', () => {
-    btnPost.removeAttribute('disabled')
-  }) 
+  const postForm = timeLineSection.querySelector("#postForm");
+  const postBox = timeLineSection.querySelector("#postBox");
+  const btnPost = timeLineSection.querySelector("#btnPost");
 
-  postForm.addEventListener('submit', (e) => {
+  postBox.addEventListener("keyup", () => {
+    btnPost.removeAttribute("disabled");
+  });
+
+  postForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const userName = firebase.auth().currentUser.displayName;
     const postText = postForm.post.value;
     const userId = firebase.auth().currentUser.uid;
-    
-          
+
     savePost(userName, postText, userId);
     postForm.reset();
-    btnPost.setAttribute('disabled', 'disabled');
+    btnPost.setAttribute("disabled", "disabled");
   });
-  
+
   /* Crear e insertar los elementos de un post en el DOM */
-  const postSection = timeLineSection.querySelector('#postSection');
-  const btnSave = timeLineSection.querySelector('#btnSave');
-  btnSave.setAttribute('class', 'hideBtn');
-  
+  const postSection = timeLineSection.querySelector("#postSection");
+
   const showPost = (doc) => {
     /* Crear los elementos de la publicación y agregar clase para estilos */
-    const postContainer = document.createElement('div');
-    postContainer.setAttribute('class', 'post-container');
+    const postContainer = document.createElement("div");
+    postContainer.setAttribute("class", "post-container");
 
-    const postBy = document.createElement('p');
-    postBy.setAttribute('class', 'post-by');
+    const postBy = document.createElement("p");
+    postBy.setAttribute("class", "post-by");
 
-    const postContent = document.createElement('p');
-    postContent.setAttribute('class', 'post-content');
+    const postContent = document.createElement("p");
+    postContent.setAttribute("class", "post-content");
 
-    const btnsContainer = document.createElement('div');
-    btnsContainer.setAttribute('class', 'buttons-container');
-    btnsContainer.setAttribute('data-id', doc.id);
+    const btnsContainer = document.createElement("div");
+    btnsContainer.setAttribute("class", "buttons-container");
+    btnsContainer.setAttribute("data-id", doc.id);
 
-    const likeBtn = document.createElement('img');
-    likeBtn.setAttribute('class', 'like-button');
-    likeBtn.setAttribute('src', 'images/like.png');
-    likeBtn.setAttribute('data-id', doc.id);
+    const likeBtn = document.createElement("img");
+    likeBtn.setAttribute("class", "like-button");
+    likeBtn.setAttribute("src", "images/like.png");
+    likeBtn.setAttribute("data-id", doc.id);
 
-    const editBtn = document.createElement('img');
-    editBtn.setAttribute('class', 'edit-button');
-    editBtn.setAttribute('src', 'images/pencil.png');
+    const editBtn = document.createElement("img");
+    editBtn.setAttribute("class", "edit-button");
+    editBtn.setAttribute("src", "images/pencil.png");
 
-    const deleteBtn = document.createElement('img');
-    deleteBtn.setAttribute('class', 'delete-button');
-    deleteBtn.setAttribute('src', 'images/bin.png');
+    const deleteBtn = document.createElement("img");
+    deleteBtn.setAttribute("class", "delete-button");
+    deleteBtn.setAttribute("src", "images/bin.png");
 
-    const likeCount = document.createElement('p');
-    likeCount.setAttribute('class', 'likesCounter');
+    const likeCount = document.createElement("p");
+    likeCount.setAttribute("class", "likesCounter");
 
     /* Agregar el texto del nombre y la publicación a los elementos */
     postBy.textContent = `Publicado por ${doc.data().user}`;
@@ -117,97 +126,126 @@ export const createTimeLineView = () => {
     /* cuando el usuario logueado realice una publicación, podrá ver los elementos de editar
     y eliminar */
     if (currentUserId === userIdPost) {
-      editBtn.style.display = 'block';
-      deleteBtn.style.display = 'block';
+      editBtn.style.display = "block";
+      deleteBtn.style.display = "block";
     }
 
     /* Función para el conteo de los likes (uno por persona) */
-    likeBtn.addEventListener('click', (e) => {
+    likeBtn.addEventListener("click", (e) => {
       const likedPost = e.target.dataset.id;
       const likesByPost = doc.data().likes;
 
       if (likesByPost.includes(currentUserId)) {
-        removeLike(currentUserId, likedPost)
-          .catch((error) => console.log(error));
+        removeLike(currentUserId, likedPost).catch((error) =>
+          console.log(error)
+        );
       } else {
-        addLike(currentUserId, likedPost)
-          .catch((error) => console.log(error));
+        addLike(currentUserId, likedPost).catch((error) => console.log(error));
       }
     });
 
     // Mostrar el conteo de like sólo cuando tenga más de un like
     if (doc.data().likes.length == 0) {
-      likeCount.style.display = 'none';
+      likeCount.style.display = "none";
     } else {
-      likeCount.style.display = 'block';
+      likeCount.style.display = "block";
     }
 
-    const hideBtn = (btn) => btn.setAttribute('class', 'hideBtn');
-    const showBtn = (btn) => btn.removeAttribute('class', 'hideBtn');
+    // const btnSave = timeLineSection.querySelector('#btnSave');
+    // btnSave.setAttribute('class', 'hideBtn');
 
-    // editar una publicación
-    editBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      // mostrar botón de guardar y ocultar de publicar
-      showBtn(btnSave);
-      hideBtn(btnPost);
-      const idPost = e.target.dataset.id;
-      const userPost = doc.data().userPost;
-      postBox.value = userPost;
+    // const hideBtn = (btn) => btn.setAttribute('class', 'hideBtn');
+    // const showBtn = (btn) => btn.removeAttribute('class', 'hideBtn');
 
-      // ejecutando boton guardar post editado
-      btnSave.addEventListener('click', (e) => {
-        e.preventDefault();
-        const newText = postBox.value;
-        updatePost(idPost, newText)
-        postContent.textContent = doc.data().userPost;
-        
-        /* postBox.value = ''; */
-        showBtn(btnPost);
-        hideBtn(btnSave);
-      });
+    // // editar una publicación
+    // editBtn.addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   // mostrar botón de guardar y ocultar de publicar
+    //   showBtn(btnSave);
+    //   hideBtn(btnPost);
+    //   const idPost = e.target.dataset.id;
+    //   const userPost = doc.data().userPost;
+    //   postBox.value = userPost;
+
+    //   // ejecutando boton guardar post editado
+    //   btnSave.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     const newText = postBox.value;
+    //     updatePost(idPost, newText)
+    //     postContent.textContent = doc.data().userPost;
+
+    //     /* postBox.value = ''; */
+    //     showBtn(btnPost);
+    //     hideBtn(btnSave);
+    //   });
+    // });
+    /* Mostrar ventana modal de editar post*/
+    editBtn.addEventListener("click", (e) => {
+      const idPost = e.target.parentElement.getAttribute("data-id");
+      containerEdit.style.display = "block";
+      modalEdit.setAttribute("data-id", idPost);
+      const editBox = timeLineSection.querySelector("#editBox");
+      const oldText = doc.data().userPost;
+      editBox.value = oldText;
     });
 
     /* Mostrar ventana modal de confirmación borrar*/
-    deleteBtn.addEventListener('click', (e) => {
-      const idPost = e.target.parentElement.getAttribute('data-id');
-      modalDiv.style.display = 'block';
-      modalDiv.setAttribute('data-id', idPost);
+    deleteBtn.addEventListener("click", (e) => {
+      const idPost = e.target.parentElement.getAttribute("data-id");
+      modalDiv.style.display = "block";
+      modalDiv.setAttribute("data-id", idPost);
     });
-    
+
     /* Fin de la función show post */
-  }; 
+  };
+
+  /* Editar publicación */
+  const modalEdit = timeLineSection.querySelector("#modalEdit");
+
+  const editPost = (e) => {
+    if (e.target.className === "edit-confirm"){
+      const idPost = e.target.parentElement.getAttribute("data-id");
+      const editBox = timeLineSection.querySelector("#editBox"); 
+      const newText = editBox.value;
+      updatePost(idPost, newText)
+      containerEdit.style.display = "none";
+    }else if (e.target.className === "cancel-edit"){
+      containerEdit.style.display = "none";
+    }
+  }
+  modalEdit.addEventListener("click", editPost);
+
 
   /* Eliminar publicación */
-  const modalDiv = timeLineSection.querySelector('#modalDiv');
+  const modalDiv = timeLineSection.querySelector("#modalDiv");
   const confirmDelete = (e) => {
-    if(e.target.className === 'delete-confirm'){
-      const idPost = e.target.parentElement.getAttribute('data-id');
+    if (e.target.className === "delete-confirm") {
+      const idPost = e.target.parentElement.getAttribute("data-id");
       deletePost(idPost);
-      modalDiv.removeAttribute('data-id');
-      modalDiv.style.display = 'none';
-    }else if(e.target.className === 'cancel-delete'){
-      modalDiv.removeAttribute('data-id');
-      modalDiv.style.display = 'none';
+      modalDiv.removeAttribute("data-id");
+      modalDiv.style.display = "none";
+    } else if (e.target.className === "cancel-delete") {
+      modalDiv.removeAttribute("data-id");
+      modalDiv.style.display = "none";
     }
   };
-  modalDiv.addEventListener('click', confirmDelete);  
-  
+  modalDiv.addEventListener("click", confirmDelete);
+
   /* Obtener los post de la base de datos y mostrarlos en el dom */
   getPosts((snapshot) => {
-    postSection.innerHTML = '';
+    postSection.innerHTML = "";
     snapshot.docs.forEach((doc) => {
       showPost(doc);
     });
   });
 
   /* Botón para que el usuario cierre sesión */
-  const logOut = timeLineSection.querySelector('#logOut');
-  logOut.addEventListener('click', (e) => {
+  const logOut = timeLineSection.querySelector("#logOut");
+  logOut.addEventListener("click", (e) => {
     e.preventDefault();
     logOutUser()
       .then(() => {
-        window.location.hash = '#/ingreso';
+        window.location.hash = "#/ingreso";
       })
       .catch((error) => {
         console.log(error);
